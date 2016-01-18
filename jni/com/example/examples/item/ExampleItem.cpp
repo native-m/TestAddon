f3c00fc68f799f9a8c9cfb6521ba239925e724b0
ThePixelCoder <thepixelmaster2354@gmail.com>
2016-01-18 01:02:52 -0600

    Pls mojang

--- jni/com/example/examples/item/ExampleItem.cpp
+++ jni/com/example/examples/item/ExampleItem.cpp
@@ -1,6 +1,6 @@
 #include "ExampleItem.h"
 
-ExampleItem::ExampleItem(const std::string& name, short itemId) : Item(name, itemId)
+ExampleItem::ExampleItem(const std::string& name, short itemId) : Item(name, itemId - 0x100) //not really sure why we have to include '- 0x100'
 {
     Item::mItems[itemId] = this; //could do another way but this is the best way
     creativeCategory = 1; //adds our item into the bottom creative inv
